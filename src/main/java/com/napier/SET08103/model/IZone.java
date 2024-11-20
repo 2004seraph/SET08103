ef6d999875301e7d067c04d91d5a3fe2a261be54
Sam Taseff <sam.taseff@proton.me>
2024-11-20 12:15:26 +0000

    Made getPopulation return richer info

--- src/main/java/com/napier/SET08103/model/IZone.java
+++ src/main/java/com/napier/SET08103/model/IZone.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface IZone {
-    public int getPopulation();
+    public PopulationInfo getPopulation();
 
     public Zone GetZoneLevel();
 }
--- src/main/java/com/napier/SET08103/model/concepts/District.java
+++ src/main/java/com/napier/SET08103/model/concepts/District.java
@@ -1,6 +1,7 @@
 package com.napier.SET08103.model.concepts;
 
 import com.napier.SET08103.model.IZone;
+import com.napier.SET08103.model.PopulationInfo;
 import com.napier.SET08103.model.Zone;
 import com.napier.SET08103.model.db.IFieldEnum;
 
@@ -29,7 +30,7 @@ public final class District implements IFieldEnum<String>, IZone {
     }
 
     @Override
-    public int getPopulation() {
+    public PopulationInfo getPopulation() {
 //        try (PreparedStatement stmt = conn.prepareStatement(
 //                "SELECT SUM(" +  + ") FROM ? WHERE ? = ?",
 //                new String[] {
@@ -44,7 +45,11 @@ public final class District implements IFieldEnum<String>, IZone {
 //
 //                return rs.getInt(1);
 //            }
-        return 0;
+        return new PopulationInfo(
+                this,
+                0,
+                0
+        );
     }
 
     @Override
