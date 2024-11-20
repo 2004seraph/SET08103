1d45617553f6154a78be1697c81c2cb7dfee0544
Sam Taseff <sam.taseff@proton.me>
2024-11-20 12:15:26 +0000

    Made getPopulation return richer info

--- src/main/java/com/napier/SET08103/model/concepts/City.java
+++ src/main/java/com/napier/SET08103/model/concepts/City.java
@@ -1,6 +1,7 @@
 package com.napier.SET08103.model.concepts;
 
 import com.napier.SET08103.model.IZone;
+import com.napier.SET08103.model.PopulationInfo;
 import com.napier.SET08103.model.Zone;
 import com.napier.SET08103.model.db.IEntity;
 
