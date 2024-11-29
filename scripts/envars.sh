25736476d5c3ce970213be939710ad9a2aebfe7e
Sam Taseff <sam.taseff@proton.me>
2024-11-29 19:30:07 +0000

    fixed docker configuration

--- scripts/envars.sh
+++ scripts/envars.sh
@@ -8,5 +8,5 @@ done <.env
 
 # Grabs some info about the project (file name and version) from the pom.xml file
 # this means the version is only specified there and updates automatically in all the docker configuration.
-export PROJECT_ARTIFACT_ID="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.artifactId}' --non-recursive exec:exec)"
-export PROJECT_VERSION="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)"
+#export PROJECT_ARTIFACT_ID="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.artifactId}' --non-recursive exec:exec)"
+#export PROJECT_VERSION="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)"
