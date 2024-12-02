601dd0bb0c2616c37d4cdd43ea85f2138302e3a8
Sam Taseff <sam.taseff@proton.me>
2024-12-02 22:05:00 +0000

    Updated Country report unit test

--- src/test/java/uk/ac/napier/SET08103/CountryReportUnitTest.java
+++ src/test/java/uk/ac/napier/SET08103/CountryReportUnitTest.java
@@ -1,15 +1,39 @@
 package uk.ac.napier.SET08103;
 
-import uk.ac.napier.SET08103.model.concepts.Country;
-import uk.ac.napier.SET08103.reports.CountryReport;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import uk.ac.napier.SET08103.model.concepts.City;
+import uk.ac.napier.SET08103.model.concepts.Continent;
+import uk.ac.napier.SET08103.model.concepts.Country;
+import uk.ac.napier.SET08103.model.concepts.Region;
+import uk.ac.napier.SET08103.reports.CountryReport;
 
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Class for testing App
  */
+@ExtendWith(MockitoExtension.class)
 public final class CountryReportUnitTest {
+
+    @Mock
+    Country mockCountry;
+
+    @Mock
+    Region mockRegion;
+
+    @Mock
+    Continent mockContinent;
+
+    @Mock
+    City mockCapital;
 
     @Test
     void printCountryReportTestNull(){
@@ -19,13 +43,30 @@ public final class CountryReportUnitTest {
     @Test
     void printCountryReportTestEmpty(){
         ArrayList<Country> countries = new ArrayList<>();
-        CountryReport.print(countries);
+
+        mockCountry = mock(Country.class);
+        when(mockCountry.getPrimaryKey()).thenReturn("Primary Key");
+        when(mockCountry.toString()).thenReturn("Country name");
+        ReflectionTestUtils.setField(mockCountry, "population", 0);
+
+        when(mockContinent.toString()).thenReturn("A continent");
+        ReflectionTestUtils.setField(mockCountry, "continent", mockContinent);
+
+        when(mockRegion.toString()).thenReturn("A region");
+        ReflectionTestUtils.setField(mockCountry, "region", mockRegion);
+
+        when(mockCapital.toString()).thenReturn("A capital city");
+        ReflectionTestUtils.setField(mockCountry, "capital", mockCapital);
+
+        countries.add(mockCountry);
+
+        assertAll(() -> CountryReport.print(countries));
     }
 
     @Test
     void printCountryReportContainsNull(){
         ArrayList<Country> countries = new ArrayList<>();
         countries.add(null);
-        CountryReport.print(countries);
+        assertAll(() -> CountryReport.print(countries));
     }
 }
